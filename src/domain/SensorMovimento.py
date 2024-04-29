1f86198cecba406f52ab6aeda01355e93c7e7f69
LeoVasc5 <leovasc5@hotmail.com>
2024-04-29 10:49:56 -0300

    Reduzido constância de movimentos e criado algoritmo mais realista para a madrugada

--- src/domain/SensorMovimento.py
+++ src/domain/SensorMovimento.py
@@ -6,41 +6,17 @@ class SensorMovimento(ModeloSensor):
     def __init__(self, id, nome, nome_bruto, fabricante, funcionalidade, tipo, unidade_medida, min_val, max_val, regular_min_val, regular_max_val, is_anomalia):
         super().__init__(id, nome, nome_bruto, fabricante, funcionalidade, tipo, unidade_medida, min_val, max_val, regular_min_val, regular_max_val, is_anomalia)
 
-    def calculate_probability(self, hour):
-        switcher = {
-            0: 0.01,   #  0h - Probabilidade muito baixa
-            1: 0.01,   #  1h - Probabilidade muito baixa
-            2: 0.01,   #  2h - Probabilidade muito baixa
-            3: 0.01,   #  3h - Probabilidade muito baixa
-            4: 0.01,   #  4h - Probabilidade muito baixa
-            5: 0.02,   #  5h - Probabilidade baixa
-            6: 0.02,   #  6h - Probabilidade baixa
-            7: 0.03,   #  7h - Probabilidade baixa
-            8: 0.03,   #  8h - Probabilidade baixa
-            9: 0.04,   #  9h - Probabilidade um pouco baixa
-            10: 0.05,  # 10h - Probabilidade um pouco baixa
-            11: 0.06,  # 11h - Probabilidade um pouco baixa
-            12: 0.07,  # 12h - Probabilidade baixa
-            13: 0.07,  # 13h - Probabilidade baixa
-            14: 0.07,  # 14h - Probabilidade baixa
-            15: 0.07,  # 15h - Probabilidade baixa
-            16: 0.07,  # 16h - Probabilidade baixa
-            17: 0.07,  # 17h - Probabilidade baixa
-            18: 0.06,  # 18h - Probabilidade um pouco baixa
-            19: 0.05,  # 19h - Probabilidade um pouco baixa
-            20: 0.04,  # 20h - Probabilidade um pouco baixa
-            21: 0.03,  # 21h - Probabilidade um pouco baixa
-            22: 0.02,  # 22h - Probabilidade baixa
-            23: 0.01   # 23h - Probabilidade muito baixa
-        }
-
-        return switcher.get(hour, 0.01)
+    def sortear_movimento(self, ultima_ocorrencia):
+        hora_atual = int(datetime.now().strftime('%H'))
+        if 0 <= hora_atual <= 7:
+            return random.random() < 0.005
+        elif ultima_ocorrencia == True:
+            return random.random() < 0.75
+        else:
+            return random.random() < 0.2
 
     def simular_dado(self, ultima_ocorrencia=None):
-        hour = datetime.now().hour
-        probability = self.calculate_probability(hour)
-
-        if random.random() < probability:
-            return True
+        if ultima_ocorrencia is not None:
+            return self.sortear_movimento(ultima_ocorrencia)
         else:
-            return False
+            return self.sortear_movimento(False)
