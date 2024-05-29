61a1451f66fa0ace1ec94b633cd36e19040093f1
KauanCavazani <kauan.cavazani@gmail.com>
2024-05-29 09:06:10 -0300

    add: conexão com iot hub

--- src/thread/thread_functions.py
+++ src/thread/thread_functions.py
@@ -1,17 +1,23 @@
 from dotenv import load_dotenv
-from service.http_client import enviar_json
 import time
 import os
+import importlib
 
 load_dotenv()
 
+USE_IOT_HUB = os.getenv('USE_IOT_HUB', 'false').lower() == 'true'
 INTERVALO_TENTATIVA_ENVIO_JSON = int(os.getenv('INTERVALO_TENTATIVA_ENVIO_JSON'))
+
+if USE_IOT_HUB:
+    module = importlib.import_module('service.iot_hub')
+else:
+    module = importlib.import_module('service.http_client')
 
 def tentar_enviar_json_periodicamente(dados):
     success = False
 
     while not success:
-        success = enviar_json(dados)
+        success = module.enviar_json(dados)
 
         if not success:
             time.sleep(INTERVALO_TENTATIVA_ENVIO_JSON)
