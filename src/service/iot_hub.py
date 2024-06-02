405ebd6b13fec1727451a05ff326749b9bd90f1f
LeoVasc5 <leovasc5@hotmail.com>
2024-06-02 19:13:08 -0300

    Implementado envio adaptado ao iot-hub

--- src/service/iot_hub.py
+++ src/service/iot_hub.py
@@ -2,7 +2,6 @@ from dotenv import load_dotenv
 import os
 import sys
 import json
-from config.logger import Logger
 from azure.iot.device import IoTHubDeviceClient, Message
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
@@ -12,14 +11,23 @@ load_dotenv()
 
 IOT_HUB_CONNECTION_STRING = os.getenv('IOT_HUB_CONNECTION_STRING')
 
+
+def formatter(dados):
+    dados.get("registros", [])
+
+    return dados
+
+
 def enviar_json(dados):
     client = IoTHubDeviceClient.create_from_connection_string(IOT_HUB_CONNECTION_STRING)
+    dados = formatter(dados)
+
     try:
         client.connect()
         json_content = json.dumps(dados)
         message = Message(json_content)
         client.send_message(message)
-        logger.log("info", f'Dados enviados para o Iot Hub: {json_content}')
+        logger.log("info", "Dados enviados para o Iot Hub")
     except Exception as ex:
         logger.log("error", f'Ocorreu um erro ao enviar os dados para o Iot Hub: {ex}')
     finally:
